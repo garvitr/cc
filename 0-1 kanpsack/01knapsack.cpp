82f9c9b846c0612ef9a89e1926cf604ca7bf4a29
Garvit Raghuvanshi <garvit.r@flipkart.com>
2020-08-08 22:10:47 +0530

    Update 01knapsack.cpp

--- 0-1 kanpsack/01knapsack.cpp
+++ 0-1 kanpsack/01knapsack.cpp
@@ -23,14 +23,14 @@ int main() {
         row(i) = weights (1->w)
         col(j) = value indexs (1->n)
         */
-        int ans[w+1][n+1] = {0};
+        long long int ans[w+1][n+1] = {0};
         int chosen[w+1][n+1] = {0};
 
         for(int weight=1;weight<=w;weight++){
             for(int index=1;index<=n;index++){
                 if(wt[index]<=weight){
-                    int ans_incl_val = ans[weight-wt[index]][index-1] + val[index];
-                    int ans_excl_val = ans[weight][index-1];
+                    long long int ans_incl_val = ans[weight-wt[index]][index-1] + val[index];
+                    long long int ans_excl_val = ans[weight][index-1];
                     chosen[weight][index] = ans_incl_val>ans_excl_val ? 1 : 0;
                     ans[weight][index] = max(ans_incl_val,ans_excl_val);
                 }else{
@@ -44,9 +44,9 @@ int main() {
         /*
         chosen numbers
         */
-        int weight = w;
+        long long int weight = w;
         int index = n;
-        int answer = 0;
+        long long int answer = 0;
         while(weight>0 && index>0){
             if(chosen[weight][index]){
                 weight  = weight - wt[index];
